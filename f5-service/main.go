ba0c4534ab466250f0f11d5006871269cfc090b2
udhos <udhos>
2018-10-17 16:30:29 -0300

    Methods for handling rules.

--- f5-service/main.go
+++ f5-service/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"runtime"
+	"strings"
 )
 
 const (
@@ -25,8 +26,9 @@ func main() {
 	}
 
 	register("/", func(w http.ResponseWriter, r *http.Request) { handlerRoot(w, r, "/") })
-	register("/v1/rule", func(w http.ResponseWriter, r *http.Request) { handlerRule(w, r, "/v1/rule") })
-	register("/v1/rule/", func(w http.ResponseWriter, r *http.Request) { handlerRule(w, r, "/v1/rule/") })
+
+	register("/v1/ff/node/", func(w http.ResponseWriter, r *http.Request) { handlerNodeF5(w, r, "/v1/ff/node/") })
+	register("/v1/at/node/", func(w http.ResponseWriter, r *http.Request) { handlerNodeA10(w, r, "/v1/at/node/") })
 
 	log.Printf("serving HTTP on TCP %s LISTEN=[%s]", addr, os.Getenv("LISTEN"))
 
@@ -63,21 +65,97 @@ func handlerRoot(w http.ResponseWriter, r *http.Request, path string) {
 	io.WriteString(w, nothing+"\n")
 }
 
-func handlerRule(w http.ResponseWriter, r *http.Request, path string) {
+func handlerNodeA10(w http.ResponseWriter, r *http.Request, path string) {
+	msg := fmt.Sprintf("handlerNodeA10: method=%s url=%s from=%s - NOT IMPLEMENTED", r.Method, r.URL.Path, r.RemoteAddr)
+	log.Print(msg)
 
-	if r.URL.Path != path {
-		sendNotFound("handlerRule", w, r)
+	notImplemented := fmt.Sprintf("not implemented: [%s]", r.URL.Path)
+
+	w.WriteHeader(http.StatusNotImplemented)
+
+	io.WriteString(w, notImplemented+"\n")
+}
+
+// /v1/ff/node/<host>/rule/<rule>
+// ^^^^^^^^^^^^
+// prefix
+func handlerNodeF5(w http.ResponseWriter, r *http.Request, path string) {
+
+	if !strings.HasPrefix(r.URL.Path, path) {
+		sendNotFound("handlerNodeF5", w, r)
 		return
 	}
 
-	msg := fmt.Sprintf("handlerHello: method=%s url=%s from=%s", r.Method, r.URL.Path, r.RemoteAddr)
+	suffix := strings.TrimPrefix(r.URL.Path, path)
+
+	msg := fmt.Sprintf("handlerNodeF5: method=%s url=%s from=%s suffix=[%s]", r.Method, r.URL.Path, r.RemoteAddr, suffix)
 	log.Print(msg)
 
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+	fields := strings.FieldsFunc(suffix, func(r rune) bool { return r == '/' })
 
-	hello := "hello rule api"
+	if len(fields) < 2 {
+		reason := fmt.Sprintf("missing path fields: %d < %d", len(fields), 2)
+		sendBadRequest("handlerNodeF5", reason, w, r)
+		return
+	}
 
-	io.WriteString(w, hello+"\n")
+	ruleField := fields[1]
+	if ruleField != "rule" {
+		reason := fmt.Sprintf("missing fule field: [%s]", ruleField)
+		sendBadRequest("handlerNodeF5", reason, w, r)
+		return
+	}
+
+	w.Header().Set("Access-Control-Allow-Origin", "*") // FIXME??
+
+	switch r.Method {
+	case http.MethodGet:
+		nodeF5RuleGet(w, r, fields)
+	case http.MethodPost:
+		nodeF5RulePost(w, r, fields)
+	case http.MethodDelete:
+		nodeF5RuleDelete(w, r, fields)
+	default:
+		w.Header().Set("Allow", "POST") // required by 405 error
+		http.Error(w, r.Method+" method not supported", 405)
+		sendNotSupported("handlerNodeF5", w, r)
+	}
+}
+
+func nodeF5RuleGet(w http.ResponseWriter, r *http.Request, fields []string) {
+	host := fields[0]
+	sendNotImplemented("nodeF5RuleGet:FIXME-WRITEME:"+host, w, r)
+}
+
+func nodeF5RulePost(w http.ResponseWriter, r *http.Request, fields []string) {
+	host := fields[0]
+	sendNotImplemented("nodeF5RulePost:FIXME-WRITEME:"+host, w, r)
+}
+
+func nodeF5RuleDelete(w http.ResponseWriter, r *http.Request, fields []string) {
+	host := fields[0]
+	if len(fields) < 3 {
+		reason := fmt.Sprintf("missing path fields: %d < %d", len(fields), 3)
+		sendBadRequest("nodeF5RuleDelete", reason, w, r)
+		return
+	}
+	sendNotImplemented("nodeF5RuleDelete:FIXME-WRITEME:"+host, w, r)
+}
+
+func sendBadRequest(label, reason string, w http.ResponseWriter, r *http.Request) {
+	msg := fmt.Sprintf("%s: method=%s url=%s from=%s - bad request: %s", label, r.Method, r.URL.Path, r.RemoteAddr, reason)
+	log.Print(msg)
+
+	http.Error(w, reason, http.StatusBadRequest) // 400
+}
+
+func sendNotSupported(label string, w http.ResponseWriter, r *http.Request) {
+	msg := fmt.Sprintf("%s: method=%s url=%s from=%s - method not supported", label, r.Method, r.URL.Path, r.RemoteAddr)
+	log.Print(msg)
+
+	w.Header().Set("Allow", "POST") // required by 405 error
+
+	http.Error(w, r.Method+" method not supported", http.StatusMethodNotAllowed) // 405
 }
 
 func sendNotFound(label string, w http.ResponseWriter, r *http.Request) {
@@ -89,4 +167,11 @@ func sendNotFound(label string, w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 
 	io.WriteString(w, notFound+"\n")
+}
+
+func sendNotImplemented(label string, w http.ResponseWriter, r *http.Request) {
+	msg := fmt.Sprintf("%s: method=%s url=%s from=%s - NOT IMPLEMENTED", label, r.Method, r.URL.Path, r.RemoteAddr)
+	log.Print(msg)
+
+	http.Error(w, label+" not implemented", http.StatusNotImplemented) // 501
 }
