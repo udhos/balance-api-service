5579e0447d6a963e17edd38922f4d5bcdd5ea96a
udhos <udhos>
2018-10-17 17:38:14 -0300

    Forward request.

--- f5-service/main.go
+++ f5-service/main.go
@@ -8,6 +8,9 @@ import (
 	"os"
 	"runtime"
 	"strings"
+
+	"github.com/e-XpertSolutions/f5-rest-client/f5"
+	"github.com/e-XpertSolutions/f5-rest-client/f5/ltm"
 )
 
 const (
@@ -86,6 +89,14 @@ func handlerNodeF5(w http.ResponseWriter, r *http.Request, path string) {
 		return
 	}
 
+	w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+
+	username, password, authOK := r.BasicAuth()
+	if !authOK {
+		http.Error(w, "not authorized", http.StatusUnauthorized) // 401
+		return
+	}
+
 	suffix := strings.TrimPrefix(r.URL.Path, path)
 
 	msg := fmt.Sprintf("handlerNodeF5: method=%s url=%s from=%s suffix=[%s]", r.Method, r.URL.Path, r.RemoteAddr, suffix)
@@ -110,11 +121,11 @@ func handlerNodeF5(w http.ResponseWriter, r *http.Request, path string) {
 
 	switch r.Method {
 	case http.MethodGet:
-		nodeF5RuleGet(w, r, fields)
+		nodeF5RuleGet(w, r, username, password, fields)
 	case http.MethodPost:
-		nodeF5RulePost(w, r, fields)
+		nodeF5RulePost(w, r, username, password, fields)
 	case http.MethodDelete:
-		nodeF5RuleDelete(w, r, fields)
+		nodeF5RuleDelete(w, r, username, password, fields)
 	default:
 		w.Header().Set("Allow", "POST") // required by 405 error
 		http.Error(w, r.Method+" method not supported", 405)
@@ -122,24 +133,64 @@ func handlerNodeF5(w http.ResponseWriter, r *http.Request, path string) {
 	}
 }
 
-func nodeF5RuleGet(w http.ResponseWriter, r *http.Request, fields []string) {
+func nodeF5RuleGet(w http.ResponseWriter, r *http.Request, username, password string, fields []string) {
 	host := fields[0]
-	sendNotImplemented("nodeF5RuleGet:FIXME-WRITEME:"+host, w, r)
+
+	f5Host := "https://" + host
+
+	log.Printf("nodeF5RuleGet: method=%s url=%s from=%s f5.NewBasicClient opening: %s", r.Method, r.URL.Path, r.RemoteAddr, f5Host)
+
+	f5Client, errOpen := f5.NewBasicClient(f5Host, username, password)
+
+	if errOpen != nil {
+		log.Printf("nodeF5RuleGet: method=%s url=%s from=%s f5.NewBasicClient: %v", r.Method, r.URL.Path, r.RemoteAddr, errOpen)
+		http.Error(w, host+" bad gateway - open", http.StatusBadGateway) // 502
+		return
+	}
+
+	f5Client.DisableCertCheck()
+
+	ltmClient := ltm.New(*f5Client)
+
+	vsConfigList, errVirtList := ltmClient.Virtual().ListAll()
+	if errVirtList != nil {
+		log.Printf("nodeF5RuleGet: method=%s url=%s from=%s virtual list: %v", r.Method, r.URL.Path, r.RemoteAddr, errVirtList)
+		http.Error(w, host+" bad gateway - virtual list", http.StatusBadGateway) // 502
+		return
+	}
+
+	poolMembers := ltmClient.PoolMembers()
+	members, errMembersList := poolMembers.ListAll()
+	if errMembersList != nil {
+		log.Printf("nodeF5RuleGet: method=%s url=%s from=%s pool members list: %v", r.Method, r.URL.Path, r.RemoteAddr, errMembersList)
+		http.Error(w, host+" bad gateway - pool members list", http.StatusBadGateway) // 502
+		return
+	}
+
+	writeStr("nodeF5RuleGet", w, fmt.Sprintf("vs: %v\nmembers:%v\n", vsConfigList, members))
 }
 
-func nodeF5RulePost(w http.ResponseWriter, r *http.Request, fields []string) {
+func writeStr(caller string, w http.ResponseWriter, s string) {
+	_, err := io.WriteString(w, s)
+	if err != nil {
+		log.Printf("%s writeStr: %v", caller, err)
+	}
+}
+
+func nodeF5RulePost(w http.ResponseWriter, r *http.Request, username, password string, fields []string) {
 	host := fields[0]
 	sendNotImplemented("nodeF5RulePost:FIXME-WRITEME:"+host, w, r)
 }
 
-func nodeF5RuleDelete(w http.ResponseWriter, r *http.Request, fields []string) {
+func nodeF5RuleDelete(w http.ResponseWriter, r *http.Request, username, password string, fields []string) {
 	host := fields[0]
 	if len(fields) < 3 {
 		reason := fmt.Sprintf("missing path fields: %d < %d", len(fields), 3)
 		sendBadRequest("nodeF5RuleDelete", reason, w, r)
 		return
 	}
-	sendNotImplemented("nodeF5RuleDelete:FIXME-WRITEME:"+host, w, r)
+	ruleName := fields[2]
+	sendNotImplemented("nodeF5RuleDelete:FIXME-WRITEME:host="+host+":rule="+ruleName, w, r)
 }
 
 func sendBadRequest(label, reason string, w http.ResponseWriter, r *http.Request) {
